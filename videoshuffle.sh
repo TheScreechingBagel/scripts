6e4439d57b3249f2fdd4401ebe51d4be0213e741
Screeching <thescreechingbagel@tutanota.com>
2022-03-09 18:56:59 -0800

    make video finding more universal, no longer only '.webm'

--- videoshuffle.sh
+++ videoshuffle.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-# Finds all the .webm files in a folder, looking through multiple layers of folders, sorts them randomly and plays in mpv
+# Finds all video type files in a folder, looking through multiple layers of folders, sorts them randomly and plays in mpv
 # by thescreechingbagel
 
-find $1 -type f -iname "*.webm" | sort -R | sed s/^/\"/ | sed s/$/\"/ | xargs mpv
+find $1 -type f | file --mime-type --no-pad --files-from - | grep --fixed-strings ': video/' | sed 's/:.*//' | sort -R | sed s/^/\"/ | sed s/$/\"/ | xargs mpv

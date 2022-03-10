52a5f106f4950514b429d4845e9ac70c3c815b72
Screeching <thescreechingbagel@tutanota.com>
2022-03-09 18:19:24 -0800

    Use 'audio' as displayed with the file command rather than a file extention (used to be .opus); makes it much more universal

--- musicshuffle.sh
+++ musicshuffle.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-# Finds all the .opus files in a folder, looking through multiple layers of folders, sorts them randomly and plays in mpv
+# Finds all "audio" (as displayed my the file command) files in a folder, looking through multiple layers of folders, sorts them randomly and plays in mpv
 # by thescreechingbagel
 
-find $1 -type f -iname "*.opus" | sort -R | sed s/^/\"/ | sed s/$/\"/ | xargs mpv
+find $1 -print0 | xargs -0 file -F '//' | awk -F '//' 'tolower($2) ~ /audio/ { print $1 }' | sort -R | sed s/^/\"/ | sed s/$/\"/ | xargs mpv

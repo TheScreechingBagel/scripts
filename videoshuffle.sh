517b7c593d33390a76b16ad93c0d1a6301c1bbf7
Screeching <thescreechingbagel@tutanota.com>
2022-04-22 12:10:12 -0700

    added "--no-resume-playback" to mpv in videoshuffle, musicshuffle had that in last commit (oops)

--- videoshuffle.sh
+++ videoshuffle.sh
@@ -2,4 +2,4 @@
 # Finds all video type files in a folder, looking through multiple layers of folders, sorts them randomly and plays in mpv
 # by thescreechingbagel
 
-find $1 -type f | file --mime-type --no-pad --files-from - | grep --fixed-strings ': video/' | sed 's/:.*//' | sort -R | sed s/^/\"/ | sed s/$/\"/ | xargs mpv
+find $1 -type f | file --mime-type --no-pad --files-from - | grep --fixed-strings ': video/' | sed 's/:.*//' | sort -R | sed s/^/\"/ | sed s/$/\"/ | xargs mpv --no-resume-playback
